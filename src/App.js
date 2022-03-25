b6897a9a20d1002e5c6f9610e4b7fbb16c7bd5e6
0cddo <0.c.dddo@gmail.com>
2022-03-25 22:00:54 +0900

    컴포넌트 생성 및 jsx 문법 규칙 공부

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
-import logo from './logo.svg';
+import Hello from './Hello';
 import './App.css';
 
 function App() {
+  const name = 'react';
+  // jsx에 style 입히기
+  const style = {
+    backgroundColor: 'black',
+    color: 'aqua',
+    fontSize: 24,
+    padding: '1rem',
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello there!</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // fragment 사용
+    <>
+      {/* 컴포넌트 일종의 UI 조각 */}
+      {/* jsx, 바벨을 이용해 자바스크립트로 변환 */}
+      <Hello
+      // 이렇게도 주석 작성 가능
+      />
+      <Hello />
+      <Hello />
+      {/* jsx 내부에서 자바스크립트 값을 보여주기위해선 {}로 감싼다 */}
+      <div style={style}>{name}</div>
+      <div className="gray-box"></div>
+    </>
   );
 }
 
---
+++ src/Hello.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+// 함수형 컴포넌트 생성
+function Hello() {
+  // jsx 문법의 규칙
+  return <div>안녕하세요</div>;
+}
+
+export default Hello;
