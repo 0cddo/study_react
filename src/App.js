9e3275b74a89a4c3142b44413aa942f4a0c4cb49
0cddo <0.c.dddo@gmail.com>
2022-03-27 15:36:35 +0900

    삼항연산자와 &&연산자를 이용한 조건부렌더링

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import Hello from './Hello';
 import Wrapper from './Wrappers';
 
+// 조건부 렌더링
+// 조건 참 거짓에 따라 결과를 다르게 보여줌
 function App() {
   return (
     <>
       <Wrapper>
-        <Hello name="react" color="red" />
+        {/* isSpecial={true} 와 isSpecial은 값을 입력하지 않아도 동일하게 true값임 */}
+        <Hello name="react" color="red" isSpecial={true} />
         <Hello color="pink" />
       </Wrapper>
     </>
--- src/Hello.js
+++ src/Hello.js
@@ -1,22 +1,24 @@
 import React from 'react';
 
-// 구조분해할당 사용해서 'props'중복 없애기
-function Hello({ color, name }) {
-  //   console.log(props);
-  //   <div style={{객체}}> -> 객체를 감싸는 {}
+// 조건부렌더링 : props isSpecial에 따라 결과 렌더링
+function Hello({ color, name, isSpecial }) {
   return (
     <div
       style={{
-        // color: color, == color 같다!
         color,
       }}
     >
+      {/* 삼항연산자 사용 : 보통 내용, 값이 달라져야할 때 사용함, 보여줘야하는 값이 다를 때 사용   */}
+      {/* {isSpecial ? <b>*삼항연산자*</b> : null} */}
+      <b>{isSpecial ? '삼항연산자인가요?' : '삼항연산자가 아닌가요?'}</b>
+      {/* &&연산자 사용: 참 거짓 조건부 렌더링은 &&연산자 사용이 좀 더 간편함  */}
+      {isSpecial && <b>&&</b>}
+      {/* 참고 : jsx에서 falsy한 값 중 null, undefined, false는 렌더링 되지 않지만 0은 렌더링 됨  */}
       안녕하세요 {name}
     </div>
   );
 }
 
-// 기본 props
 Hello.defaultProps = {
   name: '이름없음',
 };
