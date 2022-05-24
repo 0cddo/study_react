e21e73a9633fc7f5a1436d9b65f88cdd220e368a
0cddo <0.c.dddo@gmail.com>
2022-05-24 19:01:23 +0900

    useRef로 컴포넌트 안의 변수 만들기

--- src/App.js
+++ src/App.js
@@ -1,7 +1,41 @@
+import { useRef } from 'react';
 import UserList from './UserList';
 
 function App() {
-  return <UserList />;
+  const users = [
+    {
+      id: 1,
+      username: 'young eun',
+      email: 'hello0@gmail.com',
+    },
+    {
+      id: 2,
+      username: 'hyun joon',
+      email: 'hellojoon@gmail.com',
+    },
+    {
+      id: 3,
+      username: 'shim ddo',
+      email: 'hello-ddo@gmail.com',
+    },
+  ];
+
+  // 변수 값을 기억하고 싶을 때 사용
+  const nextId = useRef(4);
+
+  const onCreate = () => {
+    console.log(nextId.current); // 4
+    nextId.current += 1; // 값이 바뀐다고 해서 컴포넌트 리렌더링 되지 않음
+  };
+
+  return <UserList users={users} />;
 }
 
 export default App;
+
+// useRef 사용
+// - 컴포넌트가 리렌더링 될 때마다 특정 값을 관리 할 때 사용함
+// - setTimeout, setInterval의 id
+// - 외부라이브러리를 사용하여 생성된 인스턴스
+// - scroll 위치알아야할 때 등..
+// - useRef로 관리하는 값은 바뀌어도 컴포넌트 리렌더링안됨
--- src/UserList.js
+++ src/UserList.js
@@ -1,6 +1,5 @@
 import React from 'react';
 
-// 효율적인 방법의 배열 렌더링 (컴포넌트 새로 생성, 한 컴포넌트에 컴포넌트 여러개여도 됩니다~)
 function User({ user }) {
   return (
     <div>
@@ -9,31 +8,10 @@ function User({ user }) {
   );
 }
 
-function UserList() {
-  const users = [
-    {
-      id: 1,
-      username: 'young eun',
-      email: 'hello0@gmail.com',
-    },
-    {
-      id: 2,
-      username: 'hyun joon',
-      email: 'hellojoon@gmail.com',
-    },
-    {
-      id: 3,
-      username: 'shim ddo',
-      email: 'hello-ddo@gmail.com',
-    },
-  ];
-
+function UserList({ users }) {
   return (
     <>
       {users.map((user) => (
-        //   key : 각 원소들마다 고유값을 주어 리렌더링 성능 최적화
-        // key로 사용할 id가 없을 경우 map 콜백 함수의 두번재 파라미터 index 이용 (경고만 사라질뿐 성능적으로 좋아지는 것은 아님 )
-        // users.map((user, index) => <User key={index}/>)
         <User user={user} key={user.id} />
       ))}
     </>
