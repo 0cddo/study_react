991edb8e2d1173c201867ed7b532f76b75a45ad9
0cddo <0.c.dddo@gmail.com>
2022-06-04 18:57:32 +0900

    리듀서 immer로 구현하기

--- src/App.js
+++ src/App.js
@@ -3,35 +3,6 @@ import CreateUser from './CreateUser';
 import UserList from './UserList';
 import produce from 'immer';
 
-// * immer 라이브러리를 이용해 더 쉽게 불변성 지키기
-// window.produce = produce;
-
-// * immer 활용방법 1
-// produce(바꾸고싶은 객체 또는 배열, 어떻게 바꿀지 함수삽입)
-/* const state = {
-  number: 1,
-  dontChangeMe: 2,
-};
-
-const nextState = produce(state, (draft) => (draft.number += 1));
-console.log(nextState); */
-
-// * immer 활용방법 2
-/* const array = [
-  { id: 1, text: 'hello' },
-  { id: 2, text: '0cddo' },
-  { id: 3, text: 'kimi' },
-];
-
-const nextArray = produce(array, (draft) => {
-  draft.push({ id: 4, text: '9min' });
-  draft[0].text = draft[0].text + ' world';
-});
-
-console.log(nextArray); 
-console.log(array); // 기존 배열 변화없이 불변성 유지됨 
-*/
-
 function countActiveUsers(users) {
   console.log('활성 사용자 수를 세는 중...');
   return users.filter((user) => user.active).length;
@@ -60,25 +31,65 @@ const initialState = {
   ],
 };
 
+// * immer 사용하여 Reducer 구현
+// - 무조건 코드가 깔끔해지는건 아님, (REMOVE_USER는 깔끔해서 굳이 필요없지만 연습해서 해보자~)
+// - 필수 라이브러리 아니니까 그냥 알아두세요~~
+
+// * immer의 업데이트 함수 속성 잘 이용하면 편해요~~
+// - useState에서 까다로운 객체 관리할 때 편해요~~
+// 예시
+/* const [todo, setTodo] = useState({
+  text: 'eat pizza',
+  done: false,
+});
+
+const onClick = useCallback(() => {
+  setTodo(
+    // 첫번째 매개변수에 바로 draft함수 넣으면 업데이트에 쓰임
+    produce((draft) => {
+      draft.done = !draft.done;
+    })
+  );
+}, []); */
+
+// * immer 주의점
+// - 구형브라우저나, 리액트 네이티브에서 지원 안됨 (최신 리액트 네이티브에서는 proxy 사용 가능~)
+// - 데이터 많으면 쬐끔 느려질 수 있으나~~
+// - 필요한 곳에만 쓰자~~, 간단한거에는 쓰지말자~~
+
 function reducer(state, action) {
   switch (action.type) {
     case 'CREATE_USER':
-      return {
-        inputs: initialState.inputs,
-        users: state.users.concat(action.user),
-      };
+      return produce(state, (draft) => {
+        draft.users.push(action.user);
+      });
+    // return {
+    //   users: state.users.concat(action.user),
+    // };
+
     case 'TOGGLE_USER':
-      return {
-        ...state,
-        users: state.users.map((user) =>
-          user.id === action.id ? { ...user, active: !user.active } : user
-        ),
-      };
+      return produce(state, (draft) => {
+        const user = draft.users.find((user) => user.id === action.id);
+        user.active = !user.active;
+      });
+    // return {
+    //   ...state,
+    //   users: state.users.map((user) =>
+    //     user.id === action.id ? { ...user, active: !user.active } : user
+    //   ),
+    // };
+
     case 'REMOVE_USER':
-      return {
-        ...state,
-        users: state.users.filter((user) => user.id !== action.id),
-      };
+      return produce(state, (draft) => {
+        // splice 이용해서 배열 원소 없앰
+        const index = draft.users.findIndex((user) => user.id === action.id);
+        draft.users.splice(index, 1);
+      });
+    // return {
+    //   ...state,
+    //   users: state.users.filter((user) => user.id !== action.id),
+    // };
+
     default:
       throw new Error('Unhandled action');
   }
